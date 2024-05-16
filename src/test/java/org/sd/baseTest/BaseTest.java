d832d732d1dc16a31ca509ffc7b4d13965065a60
shubham.sahu <sahu.shubham3591@gmail.com>
2024-05-16 18:26:57 +0530

    Clean directory code added for report folder

--- src/test/java/org/sd/baseTest/BaseTest.java
+++ src/test/java/org/sd/baseTest/BaseTest.java
@@ -1,24 +1,33 @@
 package org.sd.baseTest;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.support.PageFactory;
 import org.sd.driverFactory.Base;
 import org.sd.pages.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
+
 public class BaseTest extends Base {
 
-    public  org.sd.pages.LoginPage loginPage;
-    public  org.sd.pages.ProductsPage productsPage;
-    public  org.sd.pages.CheckoutInfoPage checkoutInfoPage;
-    public  org.sd.pages.CheckoutOverviewPage checkoutOverviewPage;
-    public  org.sd.pages.OrderCompletePage orderCompletePage;
-    public  org.sd.pages.YourCartPage yourCartPage;
+    public org.sd.pages.LoginPage loginPage;
+    public org.sd.pages.ProductsPage productsPage;
+    public org.sd.pages.CheckoutInfoPage checkoutInfoPage;
+    public org.sd.pages.CheckoutOverviewPage checkoutOverviewPage;
+    public org.sd.pages.OrderCompletePage orderCompletePage;
+    public org.sd.pages.YourCartPage yourCartPage;
 
     @BeforeClass
     public void setUp() {
         launchApp("Chrome");
         PageFactoryInit();
+        try {
+            FileUtils.cleanDirectory(new File(System.getProperty("user.dir") + "//reports//"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterClass
@@ -28,7 +37,7 @@ public class BaseTest extends Base {
     }
 
 
-    protected void PageFactoryInit () {
+    protected void PageFactoryInit() {
 
         loginPage = PageFactory.initElements(getDriver(), LoginPage.class);
         checkoutInfoPage = PageFactory.initElements(getDriver(), CheckoutInfoPage.class);
