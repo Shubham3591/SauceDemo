71c76e73a151e2eda97a05ab16d7790b0d89f131
shubham.sahu <sahu.shubham3591@gmail.com>
2024-05-21 16:27:40 +0530

    Added API Practice TC

--- src/main/java/org/sd/extentReport/ExtentManager.java
+++ src/main/java/org/sd/extentReport/ExtentManager.java
@@ -14,52 +14,46 @@ import java.io.IOException;
 import java.util.Date;
 
 
-
 public class ExtentManager extends Base {
 
-	private static ExtentReports extent;
-	
-	
-	
+    public static String screenshotPath;
+    public static String screenshotName;
+    private static ExtentReports extent;
 
-	    public static ExtentReports createInstance(String fileName) {
-	        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(fileName);
-	       
-	        
-	        htmlReporter.config().setTheme(Theme.STANDARD);
-	        htmlReporter.config().setDocumentTitle(fileName);
-	        htmlReporter.config().setEncoding("utf-8");
-	        htmlReporter.config().setReportName(fileName);
-	        
-	        extent = new ExtentReports();
-	        extent.attachReporter(htmlReporter);
-	        extent.setSystemInfo("Automation Tester", "Shubham Sahu");
-	        extent.setSystemInfo("Organization", "SauceDemo");
-	        extent.setSystemInfo("Build no", "SD-1234");
-	        
-	        
-	        return extent;
-	    }
-
-	    
-	    public static String screenshotPath;
-		public static String screenshotName;
-
-	public void captureScreenshot() {
-
-		File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-
-		Date d = new Date();
-		screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
-
-		try {
-			FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "\\reports\\" + screenshotName));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		
-		}
+    public static ExtentReports createInstance(String fileName) {
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(fileName);
 
 
-	}
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setDocumentTitle(fileName);
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName(fileName);
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Automation Tester", "Shubham Sahu");
+        extent.setSystemInfo("Organization", "SauceDemo");
+        extent.setSystemInfo("Build no", "SD-1234");
+
+
+        return extent;
+    }
+
+    public void captureScreenshot() {
+
+        File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+
+        Date d = new Date();
+        screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+        try {
+            FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "\\reports\\" + screenshotName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
